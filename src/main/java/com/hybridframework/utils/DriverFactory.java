c082f359ebae415ad263c533947f540054a6ad8f
Kiran Ramesh <kiranramesh135@gmail.com>
2017-05-08 22:08:18 +0530

    Updated DriverFactory and Utilities

--- src/main/java/com/hybridframework/utils/DriverFactory.java
+++ src/main/java/com/hybridframework/utils/DriverFactory.java
@@ -1,42 +1,50 @@
 package com.hybridframework.utils;
 
+import java.net.URI;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.internal.PackageUtils;
 
 public class DriverFactory {
-	
+
 	public WebDriver driver;
-	
+
 	public WebDriver setup(WebDriver driver) {
 		if (driver == null) {
-			
+
 			// TODO Browser and OS specific driver instantiation to be added
 			// here
 
 			final URI uri;
 			final URI exe;
 
-//			System.getProperty(key)
-			System.setProperty("webdriver.chrome.driver",
-			        "src/main/resources/chrome-executables/chromedriver_win32/chromedriver");
+			// System.getProperty(key)
+			// System.setProperty("webdriver.chrome.driver",
+			// "src/main/resources/chrome-executables/chromedriver_win32/chromedriver");
 
 			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("start-maximized");
 
-			if(getOSType().contains("Windows"))
-			{
-				System.out.println("Setting chrome driver path for Windows...")
-						
+			if (getOSType().contains("Windows")) {
+				System.out.println("Setting chrome driver path for Windows...");
+				System.setProperty("webdriver.chrome.driver",
+						"src/main/resources/chrome-executables/chromedriver_win32/chromedriver.exe");
+				System.out.println(System.getProperty("webdriver.chrome.driver"));
 
 			}
+			else if(getOSType().contains("Mac")) {
+				System.out.println("Setting Chrome driver path for MAC...");
+				System.setProperty("webdriver.chrome.driver", 
+						"src/main/resources/chrome-executables/chromedriver_mac64/chromedriver");
+				Utilities.executeCMD("chmod +x " + "src/main/resources/chrome-executables/chromedriver_mac64/chromedriver");
+				System.out.println(System.getProperty("webdriver.chrome.driver"));
+			}
 
-			System.setProperty("webdriver.chrome.driver",
-			        "src/main/resources/chrome-executables/chromedriver_win32/chromedriver.exe");
-
+			
 
 			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 			driver = new ChromeDriver(capabilities);
@@ -45,19 +53,16 @@ public class DriverFactory {
 			driver.manage().window().maximize();
 			this.driver = driver;
 			return this.driver;
-		}
-		else {
+		} else {
 			return this.driver;
 		}
 	}
 
-	public String getOSType(){
-		System.out.println(System.getProperty("os.name") );
+	public String getOSType() {
+		System.out.println(System.getProperty("os.name"));
 		return System.getProperty("os.name");
 	}
-	
-	
-	
+
 	public void destroyDriver() {
 		driver.quit();
 		driver = null;
--- src/main/java/com/hybridframework/utils/Utilities.java
+++ src/main/java/com/hybridframework/utils/Utilities.java
@@ -1,9 +1,11 @@
 package com.hybridframework.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -11,7 +13,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Utilities {
-	
+
 	public static void takeScreenshot(WebDriver driver, String filename) {
 		// For Local execution
 		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
@@ -20,13 +22,12 @@ public class Utilities {
 		// Augmenter().augment(driver)).getScreenshotAs(OutputType.FILE)
 		try {
 			FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "\\Screenshots\\" + filename
-			        + System.currentTimeMillis() + ".png"));
-		}
-		catch (IOException e) {
+					+ System.currentTimeMillis() + ".png"));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String getPropertyValue(String key) {
 		File config = new File("src/main/resources/config/config.properties");
 		String value = null;
@@ -35,14 +36,44 @@ public class Utilities {
 			Properties props = new Properties();
 			props.load(reader);
 			value = props.getProperty(key);
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 		return value;
 	}
+	
+	public static void executeCMD(String cmd) {
+		String s = null;
+		
+		try {
+			
+			// run the Unix "ps -ef" command
+			// using the Runtime exec method:
+			Process p = Runtime.getRuntime().exec(cmd);
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			// read the output from the command
+			System.out.println("Here is the standard output of the command: \n");
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+			
+			// read any errors from the attempted command
+			System.out.println("Here is the standard error of the command (if any): \n");
+			while ((s = stdError.readLine()) != null) {
+				System.out.println(s);
+			
+			}
+		}
+		catch(IOException e) {
+			System.out.println("exception happened - here's what I know: ");
+			e.printStackTrace();
+			
+		}
+		
+	}
+
 }
